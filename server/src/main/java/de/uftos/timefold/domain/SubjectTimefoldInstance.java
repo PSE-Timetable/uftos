8d41608039c380cc0f05f65f6bb3a83e0e074c7b
marcelfloe <marcelfloe@gmail.com>
2024-05-23 10:01:12 +0200

    Ein paar ungenutzte Imports entfernt und ein TODO für die Datenbank-Initialisierung eingefügt.

--- server/src/main/java/de/uftos/timefold/domain/SubjectTimefoldInstance.java
+++ server/src/main/java/de/uftos/timefold/domain/SubjectTimefoldInstance.java
@@ -3,7 +3,6 @@ package de.uftos.timefold.domain;
 import ai.timefold.solver.core.api.domain.lookup.PlanningId;
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-import de.uftos.timefoldQuickstart.domain.Room;
 import lombok.Getter;
 
 import java.util.List;
