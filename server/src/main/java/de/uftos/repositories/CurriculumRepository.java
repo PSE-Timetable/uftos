6aca4db368ac6321c248f7390cbb3d1c1956768e
Marcel Flörchinger <168469433+marcelfloe@users.noreply.github.com>
2024-06-21 16:12:28 +0200

    removed duplicate file (#31)

--- server/src/main/java/de/uftos/repositories/CurriculumRepository.java
+++
@@ -1,9 +0,0 @@
-package de.uftos.repositories;
-
-import de.uftos.entities.Curriculum;
-import org.springframework.data.repository.ListCrudRepository;
-import org.springframework.data.repository.ListPagingAndSortingRepository;
-
-public interface CurriculumRepository
-    extends ListPagingAndSortingRepository<Curriculum, String>, ListCrudRepository<Curriculum, String> {
-}
