6e76bc88bd3f66a6c725493e909211100d083f71
Marco <marcodemartino23@gmail.com>
2024-06-21 12:50:31 +0200

    Created missing implementations for the solver and UCDL repositories and added a config class.

---
+++ server/src/main/java/de/uftos/repositories/UcdlRepoConfig.java
@@ -0,0 +1,35 @@
+package de.uftos.repositories;
+
+import de.uftos.repositories.solver.SolverRepository;
+import de.uftos.repositories.solver.SolverRepositoryUcdl;
+import de.uftos.repositories.ucdl.UcdlRepository;
+import de.uftos.repositories.ucdl.UcdlRepositoryImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * A class to initialize the repositories needed by UCDL and the solver.
+ */
+@Configuration
+public class UcdlRepoConfig {
+
+  /**
+   * Initializes the UCDL repository.
+   *
+   * @return the UCDL repository.
+   */
+  @Bean
+  public UcdlRepository ucdlRepository() {
+    return new UcdlRepositoryImpl();
+  }
+
+  /**
+   * Initializes the solver repository.
+   *
+   * @return the solver repository.
+   */
+  @Bean
+  public SolverRepository solverRepository() {
+    return new SolverRepositoryUcdl();
+  }
+}
