224bb2b5308868d0d858330f054b13f4421a7a25
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2024-06-26 14:52:37 +0200

    feat: add shadcn ui (#81)
    * feat: add shadcn ui
    
    * style checkbox

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+  import Checkbox from '$lib/elements/ui/checkbox/checkbox.svelte';
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<Checkbox />
