c2c6177d8a6444adb7ce46f2eafe9a25fbcde799
Lars Steudle <uojhg@student.kit.edu>
2024-06-27 18:06:00 +0200

    Added resource page layout

---
+++ web/src/lib/elements/ui/dataTable/data-table.svelte
@@ -0,0 +1,195 @@
+<script lang="ts">
+  import { createTable, Render, Subscribe, createRender } from 'svelte-headless-table';
+  import { readable } from 'svelte/store';
+  import * as Table from '$lib/elements/ui/table';
+  import DataTableActions from './data-table-actions.svelte';
+  import {
+    addPagination,
+    addSortBy,
+    addTableFilter,
+    addHiddenColumns,
+    addSelectedRows,
+  } from 'svelte-headless-table/plugins';
+  import { Button } from '$lib/elements/ui/button';
+  import ArrowUpDown from 'lucide-svelte/icons/arrow-up-down';
+  import ChevronDown from 'lucide-svelte/icons/chevron-down';
+  import { Input } from '$lib/elements/ui/input';
+  import * as DropdownMenu from '$lib/elements/ui/dropdown-menu';
+  import DataTableCheckbox from "./data-table-checkbox.svelte";
+
+  //unrelated demo data from shadcn
+  type Payment = {
+    id: string;
+    amount: number;
+    status: 'pending' | 'processing' | 'success' | 'failed';
+    email: string;
+  };
+
+  export const data: Payment[] = [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com',
+    },
+    {
+      id: '489e1d42',
+      amount: 125,
+      status: 'processing',
+      email: 'example@gmail.com',
+    },
+  ];
+
+  const table = createTable(readable(data), {
+    page: addPagination(),
+    sort: addSortBy(),
+    filter: addTableFilter({
+      fn: ({ filterValue, value }) => value.toLowerCase().includes(filterValue.toLowerCase()),
+    }),
+    hide: addHiddenColumns(),
+    select: addSelectedRows(),
+  });
+
+  //TODO: creating columns from arguments given
+  const columns = table.createColumns([
+    table.column({
+      accessor: 'id',
+      header: (_, { pluginStates }) => {
+        const { allPageRowsSelected } = pluginStates.select;
+        return createRender(DataTableCheckbox, {
+          checked: allPageRowsSelected,
+        });
+      },
+      cell: ({ row }, { pluginStates }) => {
+        const { getRowState } = pluginStates.select;
+        const { isSelected } = getRowState(row);
+ 
+        return createRender(DataTableCheckbox, {
+          checked: isSelected,
+        });
+      },
+      plugins: {
+        sort: {
+          disable: true,
+        },
+      },
+    }),
+    table.column({
+      accessor: 'amount',
+      header: 'Name',
+    }),
+    table.column({
+      accessor: 'status',
+      header: 'Gebäude',
+    }),
+    table.column({
+      accessor: 'email',
+      header: 'Kapazität',
+    }),
+    table.column({
+      accessor: ({ id }) => id,
+      header: '',
+      id: 'actions',
+      cell: ({ value }) => {
+        return createRender(DataTableActions, { id: value });
+      },
+      plugins: {
+        sort: {
+          disable: true,
+        },
+      },
+    }),
+  ]);
+
+  const { headerRows, pageRows, tableAttrs, tableBodyAttrs, pluginStates, flatColumns , rows} =
+    table.createViewModel(columns);
+  const { hasNextPage, hasPreviousPage, pageIndex } = pluginStates.page;
+  const { filterValue } = pluginStates.filter;
+  const { hiddenColumnIds } = pluginStates.hide;
+  const { selectedDataIds } = pluginStates.select;
+
+  const ids = flatColumns.map((col) => col.id);
+  let hideForId = Object.fromEntries(ids.map((id) => [id, true]));
+
+  $: $hiddenColumnIds = Object.entries(hideForId)
+    .filter(([, hide]) => !hide)
+    .map(([id]) => id);
+
+  const hidableCols = ['status', 'email', 'amount'];
+</script>
+
+<div>
+  <div class="flex items-center py-4">
+    <Input class="max-w-sm" placeholder="Suche..." type="text" bind:value={$filterValue} />
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger asChild let:builder>
+        <Button variant="outline" class="ml-auto" builders={[builder]}>
+          Filter <ChevronDown class="ml-2 h-4 w-4" />
+        </Button>
+      </DropdownMenu.Trigger>
+      <DropdownMenu.Content>
+        {#each flatColumns as col}
+          {#if hidableCols.includes(col.id)}
+            <DropdownMenu.CheckboxItem bind:checked={hideForId[col.id]}>
+              {col.header}
+            </DropdownMenu.CheckboxItem>
+          {/if}
+        {/each}
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  </div>
+  <div class="rounded-md border">
+    <Table.Root {...$tableAttrs}>
+      <Table.Header>
+        {#each $headerRows as headerRow}
+          <Subscribe rowAttrs={headerRow.attrs()}>
+            <Table.Row>
+              {#each headerRow.cells as cell (cell.id)}
+                <Subscribe attrs={cell.attrs()} let:attrs props={cell.props()} let:props>
+                  <Table.Head {...attrs} class="[&:has([role=checkbox])]:pl-3">
+                    {#if (cell.id !== 'actions' && cell.id !== "id")}
+                      <Button variant="ghost" on:click={props.sort.toggle}>
+                        <Render of={cell.render()} />
+                        <ArrowUpDown class={'ml-2 h-4 w-4'} />
+                      </Button>
+                    {:else}
+                      <Render of={cell.render()} />
+                    {/if}
+                  </Table.Head>
+                </Subscribe>
+              {/each}
+            </Table.Row>
+          </Subscribe>
+        {/each}
+      </Table.Header>
+      <Table.Body {...$tableBodyAttrs}>
+        {#each $pageRows as row (row.id)}
+          <Subscribe rowAttrs={row.attrs()} let:rowAttrs>
+            <Table.Row {...rowAttrs} {...rowAttrs}
+            data-state={$selectedDataIds[row.id] && "selected"}>
+              {#each row.cells as cell (cell.id)}
+                <Subscribe attrs={cell.attrs()} let:attrs>
+                  <Table.Cell {...attrs}>
+                    <Render of={cell.render()} />
+                  </Table.Cell>
+                </Subscribe>
+              {/each}
+            </Table.Row>
+          </Subscribe>
+        {/each}
+      </Table.Body>
+    </Table.Root>
+  </div>
+  <div class="flex items-center justify-end space-x-4 py-4">
+    <div class="flex-1 text-sm text-muted-foreground">
+      {Object.keys($selectedDataIds).length} von{" "}
+      {$rows.length} Zeile(n) ausgewählt.
+    </div>
+    <Button variant="outline" size="sm" on:click={() => ($pageIndex = $pageIndex - 1)} disabled={!$hasPreviousPage}
+      >Zurück</Button
+    >
+    <Button variant="outline" size="sm" disabled={!$hasNextPage} on:click={() => ($pageIndex = $pageIndex + 1)}
+      >Weiter</Button
+    >
+  </div>
+</div>
---
+++ web/src/routes/admin/(resources)/rooms/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import pageName from "../pageName";
+    pageName.set("Raummanagment");
+    import DataTable from "$lib/elements/ui/dataTable/data-table.svelte";
+</script>
+
+<!--not yet formatted-->
+<div class="container mx-auto py-10 bg-white w-full mt-5">
+    <DataTable />
+  </div>
+
+
+
+
+
