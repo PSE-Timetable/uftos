7a538a9eca5b8d10d5b938627a3f6ab2e1839f2b
SuperMarcomen <marcodemartino23@gmail.com>
2024-06-27 20:08:01 +0200

    Added a navbar Started creating the layout for the timetable pages Finished the timetable page for teachers

---
+++ web/src/lib/elements/ui/navbar/navbar.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let title: string = 'Title';
+  export { title };
+
+</script>
+
+<div class="h-[10%] bg-primary text-white p-4 font-medium text-2xl">
+  { title }
+  <slot/>
+</div>
--- web/src/routes/timetable/teacher/+page.svelte
+++ web/src/routes/timetable/teacher/+page.svelte
@@ -4,6 +4,10 @@
   import * as Tabs from '$lib/elements/ui/tabs';
   import Lesson from '$lib/elements/ui/lesson/lesson.svelte';
   import Timeslot from '$lib/elements/ui/timeslot/timeslot.svelte';
+  import Button from '$lib/elements/ui/button/button.svelte';
+  import DataTable from "$lib/elements/ui/dataTable/data-table.svelte";
+  import Navbar from "$lib/elements/ui/navbar/navbar.svelte";
+
 
   const classes = [
     { value: '5A', label: '5A' },
@@ -14,70 +18,82 @@
   ];
 </script>
 
-<div class="flex flex-row w-full justify-between">
-  <Tabs.Root class="w-[400px]" value="account">
-    <Tabs.List class="grid w-full grid-cols-2">
-      <Tabs.Trigger value="account">Account</Tabs.Trigger>
-      <Tabs.Trigger value="password">Password</Tabs.Trigger>
-    </Tabs.List>
-    <Tabs.Content value="Schüler">
-      der
-    </Tabs.Content>
-    <Tabs.Content value="password">
+<div class="flex flex-col gap-2">
+  <div class="flex flex-row w-full justify-between">
+    <Tabs.Root class="w-[20%] min-w-[200px]" value="class">
+      <Tabs.List class="grid w-full grid-cols-3">
+        <Tabs.Trigger value="class">Klasse</Tabs.Trigger>
+        <Tabs.Trigger value="teacher">Lehrer</Tabs.Trigger>
+        <Tabs.Trigger value="room">Raum</Tabs.Trigger>
+      </Tabs.List>
 
-    </Tabs.Content>
-  </Tabs.Root>
+    </Tabs.Root>
 
-  <Lesson color="orange" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <div class="flex flex-row items-baseline gap-2">
+      <label class="font-medium" for="chosenResource">Schüler</label>
+      <Select.Root portal={null}>
+        <Select.Trigger class="w-[10%] min-w-[200px]">
+          <Select.Value placeholder="Wähle eine Klasse aus" />
+        </Select.Trigger>
+        <Select.Content>
+          <Select.Group>
+            {#each classes as classItem}
+              <Select.Item value={classItem.value} label={classItem.label}>
+                {classItem.label}
+              </Select.Item>
+            {/each}
+          </Select.Group>
+        </Select.Content>
+        <Select.Input name="chosenResource" />
+      </Select.Root>
+    </div>
+  </div>
 
-  <Select.Root portal={null}>
-    <Select.Trigger class="w-[200px]">
-      <Select.Value placeholder="Wähle eine Klasse aus" />
-    </Select.Trigger>
-    <Select.Content>
-      <Select.Group>
-        {#each classes as classItem}
-          <Select.Item value={classItem.value} label={classItem.label}>
-            {classItem.label}
-          </Select.Item>
-        {/each}
-      </Select.Group>
-    </Select.Content>
-    <Select.Input name="favoriteFruit" />
-  </Select.Root>
-</div>
-<Timeslot />
+  <!-- These empty divs are needed so that VITE doesn't let these bad apples fall.
+       There has to be a better fix!
+  -->
+  <div class="row-span-2"></div>
+  <div class="row-span-3"></div>
+  <div class="row-span-4"></div>
 
-<div class="h-[100px]"></div>
-<!-- These empty divs are needed so that VITE doesn't let these bad apples fall.
-     There has to be a better fix!
--->
-<div class="row-span-3"></div>
-<div class="row-span-4"></div>
+  <div
+    class="inline-grid grid-flow-row gap-2 grid-cols-[max-content,repeat(5,1fr)] bg-white p-4 rounded-2xl w-full">
+    <Timeslot />
+    <Lesson color="orange" leftCorner="Tur" length={2} rightCorner="102" title="Deutsch" />
+    <Lesson color="purple" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="red" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="green" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Timeslot begin="8:35" end="9:20" index="2." />
+    <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="aquamarine" leftCorner="Tur" length={2} rightCorner="102" title="Deutsch" />
+    <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Timeslot begin="9:25" end="10:10" index="3." />
+    <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="lightBlue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="brown" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <div class="col-span-full"></div>
+    <Timeslot begin="10:30" end="11:15" index="4." />
+    <Lesson color="lightBlue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="brown" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="yellow" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Timeslot begin="11:15" end="12:00" index="5." />
+    <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="red" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="lightBlue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="brown" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="yellow" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <div class="col-span-full"></div>
 
-<div
-  class="inline-grid grid-flow-row gap-2 grid-cols-[max-content,repeat(5,1fr)] bg-white p-8 rounded-3xl w-full">
-  <Timeslot />
-  <Lesson color="orange" leftCorner="Tur" length={3} rightCorner="102" title="Deutsch" />
-  <Lesson color="purple" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="red" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="green" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Timeslot begin="8:35" end="9:20" index="2." />
-  <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Timeslot begin="9:25" end="10:10" index="3." />
-  <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="lightBlue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="lightBlue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="brown" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <div class="col-span-full"></div>
-  <Timeslot begin="10:30" end="11:15" index="4." />
-  <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="lightBlue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="brown" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="yellow" leftCorner="Tur" rightCorner="102" title="Deutsch" />
-  <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Timeslot begin="12:20" end="13:05" index="6." />
+    <Lesson color="aquamarine" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="orange" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="brown" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="lightBlue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+    <Lesson color="blue" leftCorner="Tur" rightCorner="102" title="Deutsch" />
+  </div>
 </div>
