e4ba0899fc5eaa2bd96303949df6082f1f308cd4
uhkfc <daniel.dietzler@student.kit.edu>
2024-07-01 18:30:27 +0200

    feat: constraint instances

--- server/src/main/java/de/uftos/entities/ConstraintArgument.java
+++ server/src/main/java/de/uftos/entities/ConstraintArgument.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * The database table for constraint arguments.
@@ -13,6 +14,7 @@ import lombok.Data;
  */
 @Entity(name = "constraint_argument")
 @Data
+@NoArgsConstructor
 public class ConstraintArgument {
   @Id
   @GeneratedValue(strategy = GenerationType.UUID)
@@ -20,4 +22,9 @@ public class ConstraintArgument {
 
   private String name;
   private String value;
+
+  public ConstraintArgument(String name, String value) {
+    this.name = name;
+    this.value = value;
+  }
 }
