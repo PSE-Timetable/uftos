d5ca824a4c395a6490b17cfe3bd38ec7972b6b19
Marco <marcodemartino23@gmail.com>
2024-07-03 11:51:15 +0200

    Checkstyle 2.0

--- server/src/main/java/de/uftos/builders/SpecificationBuilder.java
+++ server/src/main/java/de/uftos/builders/SpecificationBuilder.java
@@ -35,7 +35,7 @@ public class SpecificationBuilder<T> {
    * @param param     The optional parameter value to filter by.
    * @param paramName The name of the parameter to filter on.
    * @return The current instance of {@code SpecificationBuilder} with the
-   *        OR filter added if the parameter is present.
+   *     OR filter added if the parameter is present.
    */
   public SpecificationBuilder<T> addOptionalOr(Optional<String> param, String paramName) {
     if (param.isEmpty()) {
@@ -53,7 +53,7 @@ public class SpecificationBuilder<T> {
    * @param relationName   The name of the relation to join.
    * @param attributeName  The name of the attribute in the joined relation to filter on.
    * @return The current instance of {@code SpecificationBuilder} with the join filter
-   *        added if the attribute value array is present.
+   *     added if the attribute value array is present.
    */
   public SpecificationBuilder<T> addOptionalJoinFilter(Optional<String[]> attributeValue,
                                                        String relationName, String attributeName) {
