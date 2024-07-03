d7fd75ff9f8d13522def10a206cb4b8657325476
Marco <marcodemartino23@gmail.com>
2024-07-03 12:47:28 +0200

    Refactored the tests Now using a JSON builder library

--- server/src/test/java/de/uftos/controller/StudentControllerTest.java
+++ server/src/test/java/de/uftos/controller/StudentControllerTest.java
@@ -5,143 +5,80 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.notNullValue;
 
 import io.restassured.http.ContentType;
+import java.util.Collections;
 import java.util.List;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 class StudentControllerTest {
 
-  static String karenId;
-  static String jasonId;
+  private static final String FIRST_STUDENT_FIRST_NAME = "Karen";
+  private static final String FIRST_STUDENT_LAST_NAME = "Musterman";
+  private static final String SECOND_STUDENT_FIRST_NAME = "Jason";
+  private static final String SECOND_STUDENT_LAST_NAME = "Musterman";
+  private static final String TAG_NAME = "Sehebehinderung";
+
+  static String firstStudent;
+  static String secondStudent;
   static String tagId;
 
   @BeforeAll
-  static void createTestStudents() {
+  static void createTestStudents() throws JSONException {
     tagId = given().contentType(ContentType.JSON)
-        .body("""
-                    {
-                      "tagName": "Sehebehinderung"
-                    }""")
+        .body(generateTagJson(TAG_NAME))
         .when()
         .post("/tags")
         .then()
         .statusCode(200)
         .body("id", notNullValue())
-        .body("name", equalTo("Sehebehinderung"))
-        .extract()
-        .body().jsonPath().getString("id");
-    System.out.println("tag id: " + tagId);
-
-    StringBuilder builder = new StringBuilder();
-    builder.append("{").append("\"firstName\": \"Jason\",").append("\"lastName\": \"Musterman\",").append("\"tagIds\": [\"").append(tagId).append("\"]}");
-    jasonId = given().contentType(ContentType.JSON)
-        .body(builder.toString())
-        .when()
-        .post("/students")
-        .then()
-        .statusCode(200)
-        .body("id", notNullValue())
-        .body("firstName", equalTo("Jason"))
-        .body("lastName", equalTo("Musterman"))
+        .body("name", equalTo(TAG_NAME))
         .log().ifValidationFails()
         .extract()
         .body().jsonPath().getString("id");
 
-    karenId = given().contentType(ContentType.JSON)
-        .body("""
-                    {
-                      "firstName": "Karen",
-                      "lastName": "Musterman",
-                      "tagIds": []
-                    }""")
+    secondStudent = given().contentType(ContentType.JSON)
+        .body(generateStudentJson(SECOND_STUDENT_FIRST_NAME, SECOND_STUDENT_LAST_NAME,
+            List.of(tagId)))
         .when()
         .post("/students")
         .then()
         .statusCode(200)
         .body("id", notNullValue())
-        .body("firstName", equalTo("Karen"))
-        .body("lastName", equalTo("Musterman"))
+        .body("firstName", equalTo(SECOND_STUDENT_FIRST_NAME))
+        .body("lastName", equalTo(FIRST_STUDENT_LAST_NAME))
+        .log().ifValidationFails()
         .extract()
         .body().jsonPath().getString("id");
-  }
 
-  @Test
-  void getAllStudentsTest() {
-    given().contentType(ContentType.JSON)
-        .body("""
-                    {
-                      "page": 0,
-                      "size": 10,
-                      "sort": [
-                        "string"
-                      ]
-                    }""")
+    firstStudent = given().contentType(ContentType.JSON)
+        .body(generateStudentJson(FIRST_STUDENT_FIRST_NAME, FIRST_STUDENT_LAST_NAME,
+            Collections.emptyList()))
         .when()
-        .get("/students")
+        .post("/students")
         .then()
         .statusCode(200)
-        .body("totalElements", equalTo(2))
-        .log()
-        .ifValidationFails();
-  }
-
-  @Test
-  void getStudentsWithTagTest() {
-    given().contentType(ContentType.JSON)
-        .body("""
-                    {
-                      "page": 0,
-                      "size": 10,
-                      "sort": [
-                        "string"
-                      ]
-                    }""")
-        .param("tags", List.of(tagId))
-        .when()
-        .get("/students")
-        .then()
-        .statusCode(200)
-        //.body("totalElements", equalTo(1))
-        .body("content[0].id", equalTo(jasonId))
-        .log()
-        .ifValidationFails();
-  }
-
-  @Test
-  void getStudentsWithNameTest() {
-    given().contentType(ContentType.JSON)
-        .body("""
-                    {
-                      "page": 0,
-                      "size": 10,
-                      "sort": [
-                        "string"
-                      ]
-                    }""")
-        .param("firstName", "Karen")
-        .when()
-        .get("/students")
-        .then()
-        .statusCode(200)
-        //.body("totalElements", equalTo(1))
-        .body("content[0].id", equalTo(karenId))
-        .log()
-        .ifValidationFails();
-
+        .body("id", notNullValue())
+        .body("firstName", equalTo(FIRST_STUDENT_FIRST_NAME))
+        .body("lastName", equalTo(FIRST_STUDENT_LAST_NAME))
+        .log().ifValidationFails()
+        .extract()
+        .body().jsonPath().getString("id");
   }
 
   @AfterAll
   static void deleteCreatedStudents() {
     given().contentType(ContentType.JSON)
         .when()
-        .delete("/students/{id}", jasonId)
+        .delete("/students/{id}", firstStudent)
         .then()
         .statusCode(200);
 
     given().contentType(ContentType.JSON)
         .when()
-        .delete("/students/{id}", karenId)
+        .delete("/students/{id}", secondStudent)
         .then()
         .statusCode(200);
 
@@ -150,5 +87,71 @@ class StudentControllerTest {
         .delete("/tags/{id}", tagId)
         .then()
         .statusCode(200);
+  }
+
+  private static String generateStudentJson(String firstName, String lastName, List<String> tags)
+      throws JSONException {
+    return new JSONObject()
+        .put("firstName", firstName)
+        .put("lastName", lastName)
+        .put("tags", tags)
+        .toString();
+  }
+
+  private static String generateTagJson(String name)
+      throws JSONException {
+    return new JSONObject()
+        .put("tagName", name)
+        .toString();
+  }
+
+  private static String generatePageJson(int page, int size, List<String> sort)
+      throws JSONException {
+    return new JSONObject()
+        .put("page", page)
+        .put("size", size)
+        .put("sort", sort)
+        .toString();
+  }
+
+  @Test
+  void getAllStudentsTest() throws JSONException {
+    given().contentType(ContentType.JSON)
+        .body(generatePageJson(0, 10, Collections.emptyList()))
+        .when()
+        .get("/students")
+        .then()
+        .statusCode(200)
+        .body("totalElements", equalTo(2))
+        .log().ifValidationFails();
+  }
+
+  @Test
+  void getStudentsWithTagTest() throws JSONException {
+    given().contentType(ContentType.JSON)
+        .body(generatePageJson(0, 10, Collections.emptyList()))
+        .param("tags", List.of(tagId))
+        .when()
+        .get("/students")
+        .then()
+        .statusCode(200)
+        .body("totalElements", equalTo(1))
+        .body("content[0].id", equalTo(secondStudent))
+        .log().ifValidationFails();
+  }
+
+  @Test
+  void getStudentsWithNameTest() throws JSONException {
+    given().contentType(ContentType.JSON)
+        .body(generatePageJson(0, 10, Collections.emptyList()))
+        .param("firstName", FIRST_STUDENT_FIRST_NAME)
+        .when()
+        .get("/students")
+        .then()
+        .statusCode(200)
+        .body("totalElements", equalTo(1))
+        .body("content[0].id", equalTo(firstStudent))
+        .log().ifValidationFails();
+
   }
 }
