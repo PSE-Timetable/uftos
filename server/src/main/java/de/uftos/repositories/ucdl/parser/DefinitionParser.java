0fe3667c96a4045c836cff9aa3f04acdcb49ac50
marcelfloe <marcelfloe@gmail.com>
2024-07-04 11:55:27 +0200

    Replaced javacc classes.

--- server/src/main/java/de/uftos/repositories/ucdl/parser/DefinitionParser.java
+++ server/src/main/java/de/uftos/repositories/ucdl/parser/DefinitionParser.java
@@ -1,10 +1,13 @@
 package de.uftos.repositories.ucdl.parser;
 
+import de.uftos.dto.ucdl.UcdlToken;
 import de.uftos.dto.ucdl.ast.AbstractSyntaxTreeDto;
 import de.uftos.dto.ucdl.ast.ValueDto;
+import de.uftos.repositories.ucdl.parser.javacc.SyntaxChecker;
 
 public class DefinitionParser {
   public static AbstractSyntaxTreeDto parseDefinition(String definition) {
-    return new ValueDto<String>(definition);
+    System.out.println(SyntaxChecker.parseString(definition));
+    return new ValueDto<String>(UcdlToken.RESOURCE, definition);
   }
 }
