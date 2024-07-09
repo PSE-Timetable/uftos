d924eba3473d306e1d621f4a659b7be7857e4768
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2024-07-09 22:52:54 +0200

    feat: working hours (#104)
    * Started creating the component for the working hours
    
    * Added (and styled) scrollbar to the select component
    
    * Automated the creation of the working hour component (The plus button is currently broken)
    Changed accent foreground color to white
    
    * The plus button is now working
    
    * Now the delete button too is working
    
    * Added instances of WorkingHours for the entire week
    
    * chore: cleanup (not at 3am)
    
    ---------
    
    Co-authored-by: Marco <marcodemartino23@gmail.com>

---
+++ web/src/routes/admin/(resources)/teachers/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import WorkingHours from '$lib/components/ui/working-hours/working-hours-day-container.svelte';
+  import { WeekDay } from '$lib/utils';
+</script>
+
+123
+<div class="h-[200px]"></div>
+<div class="flex flex-col gap-2">
+  <WorkingHours weekDay={WeekDay.monday} />
+  <WorkingHours weekDay={WeekDay.tuesday} />
+  <WorkingHours weekDay={WeekDay.wednesday} />
+  <WorkingHours weekDay={WeekDay.thursday} />
+  <WorkingHours weekDay={WeekDay.friday} />
+</div>
