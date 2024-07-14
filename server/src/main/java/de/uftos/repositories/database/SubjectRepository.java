4f75ede80ce297b795254fe38039ec4132b9bf3f
Demartino Marco <marcodemartino23@gmail.com>
2024-07-14 13:42:46 +0200

    Implemented filter for subjects (#129)
    Written unit tests and e2e tests for the subject service and subject endpoint

--- server/src/main/java/de/uftos/repositories/database/SubjectRepository.java
+++ server/src/main/java/de/uftos/repositories/database/SubjectRepository.java
@@ -1,6 +1,7 @@
 package de.uftos.repositories.database;
 
 import de.uftos.entities.Subject;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.ListPagingAndSortingRepository;
 
@@ -8,5 +9,6 @@ import org.springframework.data.repository.ListPagingAndSortingRepository;
  * The repository for accessing the subject database table.
  */
 public interface SubjectRepository
-    extends ListPagingAndSortingRepository<Subject, String>, ListCrudRepository<Subject, String> {
+    extends ListPagingAndSortingRepository<Subject, String>, ListCrudRepository<Subject, String>,
+    JpaSpecificationExecutor<Subject> {
 }
