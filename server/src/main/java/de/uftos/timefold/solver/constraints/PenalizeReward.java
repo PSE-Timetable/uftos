568872db8a5442b3f4189999effd6bbfa10fb008
marcelfloe <marcelfloe@gmail.com>
2024-07-14 17:52:29 +0200

    Removed old unnecessary classes.

--- server/src/main/java/de/uftos/timefold/solver/constraints/PenalizeReward.java
+++
@@ -1,6 +0,0 @@
-package de.uftos.timefold.solver.constraints;
-
-public enum PenalizeReward {
-    PENALIZE,
-    REWARD
-}
--- server/src/main/java/de/uftos/timefold/solver/constraints/TeacherCollisionConstraint.java
+++
@@ -1,4 +0,0 @@
-package de.uftos.timefold.solver.constraints;
-
-public class TeacherCollisionConstraint {
-}
