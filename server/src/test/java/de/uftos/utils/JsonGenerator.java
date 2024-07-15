051c383d82efeccc31aa949818ea345b4f8c2042
SuperMarcomen <marcodemartino23@gmail.com>
2024-07-15 20:49:46 +0200

    Checkstyle

--- server/src/test/java/de/uftos/utils/JsonGenerator.java
+++ server/src/test/java/de/uftos/utils/JsonGenerator.java
@@ -5,8 +5,20 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+/**
+ * This class helps to generate the JSON needed for the E2E tests.
+ */
 public class JsonGenerator {
 
+  /**
+   * Generates the student JSON.
+   *
+   * @param firstName The first name of the student
+   * @param lastName  The last name of the student
+   * @param tags      The ids of the tags the student has
+   * @return The requested JSON
+   * @throws JSONException If something is malformed.
+   */
   public static String generateStudentJson(String firstName, String lastName, List<String> tags)
       throws JSONException {
     JSONArray jsonArray = new JSONArray();
@@ -18,6 +30,13 @@ public class JsonGenerator {
         .toString();
   }
 
+  /**
+   * Generates the subject JSON.
+   *
+   * @param name The name of the subject
+   * @return The requested JSON
+   * @throws JSONException If something is malformed.
+   */
   public static String generateSubjectJson(String name)
       throws JSONException {
     return new JSONObject()
@@ -26,6 +45,13 @@ public class JsonGenerator {
         .toString();
   }
 
+  /**
+   * Generates the tag JSON.
+   *
+   * @param name The name of the tag
+   * @return The requested JSON
+   * @throws JSONException If something is malformed.
+   */
   public static String generateTagJson(String name)
       throws JSONException {
     return new JSONObject()
@@ -33,6 +59,15 @@ public class JsonGenerator {
         .toString();
   }
 
+  /**
+   * Generates the page JSON.
+   *
+   * @param page The page number
+   * @param size The size of the page
+   * @param sort How the data should be sorted
+   * @return The requested JSON
+   * @throws JSONException If something is malformed.
+   */
   public static String generatePageJson(int page, int size, List<String> sort)
       throws JSONException {
     JSONArray jsonArray = new JSONArray();
