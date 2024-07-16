213f3b18e6c8e0e028f5275ae80940f2189400c5
marcelfloe <marcelfloe@gmail.com>
2024-07-16 11:10:53 +0200

    Found unexpected Exception. Only thrown on invalid input, but should be caught earlier and forwarded to the user as ParseException.

--- server/src/main/java/de/uftos/repositories/ucdl/parser/DefinitionParser.java
+++ server/src/main/java/de/uftos/repositories/ucdl/parser/DefinitionParser.java
@@ -18,6 +18,7 @@ import java.util.HashMap;
 import java.util.List;
 
 //TODO: !!!REFACTOR!!!
+//todo: throws ClassCastException on invalid Input "NumberSet NumberSet" (e.g.: "3 in [1,2,3][1,2,3]")
 
 /**
  * This class parses the ucdl-code of the "definition"-field of a constraint definition and returns
