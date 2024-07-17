1bad5fa1bd6301f06ee9a843deb9e99d94934a70
Jan Diester <jan.diester1@gmail.com>
2024-07-17 11:15:30 +0200

    Added student group e2e test

--- server/src/test/java/de/uftos/e2e/StudentGroupsTest.java
+++ server/src/test/java/de/uftos/e2e/StudentGroupsTest.java
@@ -1,6 +1,8 @@
 package de.uftos.e2e;
 
+import static de.uftos.utils.JsonGenerator.generateGradeJson;
 import static de.uftos.utils.JsonGenerator.generatePageJson;
+import static de.uftos.utils.JsonGenerator.generateStudentGroupJson;
 import static de.uftos.utils.JsonGenerator.generateStudentJson;
 import static de.uftos.utils.JsonGenerator.generateTagJson;
 import static io.restassured.RestAssured.given;
@@ -15,17 +17,20 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-class StudentsTest {
-
-  private static final String FIRST_STUDENT_FIRST_NAME = "Karen";
-  private static final String FIRST_STUDENT_LAST_NAME = "Musterman";
-  private static final String SECOND_STUDENT_FIRST_NAME = "Jason";
-  private static final String SECOND_STUDENT_LAST_NAME = "Musterman";
+public class StudentGroupsTest {
+  private static final String FIRST_STUDENT_GROUP_NAME = "5-Ethik";
+  private static final String SECOND_STUDENT_GROUP_NAME = "5-Religion";
+  private static final String STUDENT_FIRST_NAME = "Max";
+  private static final String STUDENT_LAST_NAME = "Mustermann";
+  private static final String GRADE_NAME = "5";
   private static final String TAG_NAME = "Sehbehinderung";
 
-  static String firstStudent;
-  static String secondStudent;
+  static String firstStudentGroup;
+  static String secondStudentGroup;
+  static String studentId;
+  static String gradeId;
   static String tagId;
+
 
   @BeforeAll
   static void createTestStudents() throws JSONException {
@@ -41,46 +46,70 @@ class StudentsTest {
         .extract()
         .body().jsonPath().getString("id");
 
-    secondStudent = given().contentType(ContentType.JSON)
-        .body(generateStudentJson(SECOND_STUDENT_FIRST_NAME, SECOND_STUDENT_LAST_NAME,
-            List.of(tagId)))
+    studentId = given().contentType(ContentType.JSON)
+        .body(generateStudentJson(STUDENT_FIRST_NAME, STUDENT_LAST_NAME, Collections.emptyList()))
         .when()
         .post("/students")
         .then()
         .statusCode(200)
         .body("id", notNullValue())
-        .body("firstName", equalTo(SECOND_STUDENT_FIRST_NAME))
-        .body("lastName", equalTo(SECOND_STUDENT_LAST_NAME))
+        .body("firstName", equalTo(STUDENT_FIRST_NAME))
+        .body("lastName", equalTo(STUDENT_LAST_NAME))
         .log().ifValidationFails()
         .extract()
         .body().jsonPath().getString("id");
 
-    firstStudent = given().contentType(ContentType.JSON)
-        .body(generateStudentJson(FIRST_STUDENT_FIRST_NAME, FIRST_STUDENT_LAST_NAME,
-            Collections.emptyList()))
+    gradeId = given().contentType(ContentType.JSON)
+        .body(generateGradeJson(GRADE_NAME, Collections.emptyList(), Collections.emptyList()))
         .when()
-        .post("/students")
+        .post("/grades")
         .then()
         .statusCode(200)
         .body("id", notNullValue())
-        .body("firstName", equalTo(FIRST_STUDENT_FIRST_NAME))
-        .body("lastName", equalTo(FIRST_STUDENT_LAST_NAME))
+        .body("name", equalTo(GRADE_NAME))
+        .log().ifValidationFails()
+        .extract()
+        .body().jsonPath().getString("id");
+
+
+    firstStudentGroup = given().contentType(ContentType.JSON)
+        .body(generateStudentGroupJson(FIRST_STUDENT_GROUP_NAME, Collections.emptyList(),
+            Collections.emptyList(), Collections.emptyList()))
+        .when()
+        .post("/student-groups")
+        .then()
+        .statusCode(200)
+        .body("id", notNullValue())
+        .body("name", equalTo(FIRST_STUDENT_GROUP_NAME))
+        .log().ifValidationFails()
+        .extract()
+        .body().jsonPath().getString("id");
+
+    secondStudentGroup = given().contentType(ContentType.JSON)
+        .body(generateStudentGroupJson(SECOND_STUDENT_GROUP_NAME, List.of(studentId),
+            List.of(gradeId), List.of(tagId)))
+        .when()
+        .post("/student-groups")
+        .then()
+        .statusCode(200)
+        .body("id", notNullValue())
+        .body("name", equalTo(SECOND_STUDENT_GROUP_NAME))
         .log().ifValidationFails()
         .extract()
         .body().jsonPath().getString("id");
   }
 
   @AfterAll
-  static void deleteCreatedStudents() {
+  static void deleteCreatedEntities() {
     given().contentType(ContentType.JSON)
         .when()
-        .delete("/students/{id}", firstStudent)
+        .delete("/student-groups/{id}", firstStudentGroup)
         .then()
         .statusCode(200);
 
     given().contentType(ContentType.JSON)
         .when()
-        .delete("/students/{id}", secondStudent)
+        .delete("/student-groups/{id}", secondStudentGroup)
         .then()
         .statusCode(200);
 
@@ -92,11 +121,11 @@ class StudentsTest {
   }
 
   @Test
-  void getAllStudents() throws JSONException {
+  void getAllStudentGroups() throws JSONException {
     given().contentType(ContentType.JSON)
         .body(generatePageJson(0, 10, Collections.emptyList()))
         .when()
-        .get("/students")
+        .get("/student-groups")
         .then()
         .statusCode(200)
         .body("totalElements", equalTo(2))
@@ -109,11 +138,11 @@ class StudentsTest {
         .body(generatePageJson(0, 10, Collections.emptyList()))
         .param("tags", List.of(tagId))
         .when()
-        .get("/students")
+        .get("/student-groups")
         .then()
         .statusCode(200)
         .body("totalElements", equalTo(1))
-        .body("content[0].id", equalTo(secondStudent))
+        .body("content[0].id", equalTo(secondStudentGroup))
         .log().ifValidationFails();
   }
 
@@ -121,14 +150,13 @@ class StudentsTest {
   void getStudentsWithName() throws JSONException {
     given().contentType(ContentType.JSON)
         .body(generatePageJson(0, 10, Collections.emptyList()))
-        .param("firstName", FIRST_STUDENT_FIRST_NAME)
+        .param("name", FIRST_STUDENT_GROUP_NAME)
         .when()
-        .get("/students")
+        .get("/student-groups")
         .then()
         .statusCode(200)
         .body("totalElements", equalTo(1))
-        .body("content[0].id", equalTo(firstStudent))
+        .body("content[0].id", equalTo(firstStudentGroup))
         .log().ifValidationFails();
-
   }
 }
