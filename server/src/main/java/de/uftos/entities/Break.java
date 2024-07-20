9760eff3d60c8faaee295fc8643ca61299ac02ba
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2024-07-20 12:34:21 +0200

    chore: fix checkstyle (#162)

--- server/src/main/java/de/uftos/entities/Break.java
+++ server/src/main/java/de/uftos/entities/Break.java
@@ -6,14 +6,20 @@ import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * The timetable break object.
+ * Represents a break in the displayed timetable.
+ */
 @Data
 @AllArgsConstructor
 public class Break {
   @NotNull
   private boolean isLong;
+
   @PositiveOrZero
   @NotNull
   private int afterSlot;
+
   @Positive
   @NotNull
   private int length;
