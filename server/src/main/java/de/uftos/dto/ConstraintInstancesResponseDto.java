9fa7c978add04460b4847f9e41109feffa189f13
Demartino Marco <marcodemartino23@gmail.com>
2024-07-22 19:34:37 +0200

    Cleaned up the ConstraintInstancesResponseDto

--- server/src/main/java/de/uftos/dto/ConstraintInstancesResponseDto.java
+++ server/src/main/java/de/uftos/dto/ConstraintInstancesResponseDto.java
@@ -1,6 +1,10 @@
 package de.uftos.dto;
 
+import de.uftos.dto.solver.RewardPenalize;
+import de.uftos.entities.ConstraintArgument;
 import de.uftos.entities.ConstraintInstance;
+import de.uftos.entities.ConstraintParameter;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 
@@ -10,7 +14,30 @@ import java.util.List;
  * @param constraintInstances the instances of the constraints
  * @param displayNames        the display names of the arguments of the constraint instances
  */
-public record ConstraintInstancesResponseDto(@NotNull List<ConstraintInstance> constraintInstances,
-                                             @NotNull
-                                             List<ConstraintArgumentDisplayName> displayNames) {
+public record ConstraintInstancesResponseDto(@NotNull List<SlimInstance> constraintInstances,
+                                             @NotNull List<ConstraintArgumentDisplayName> displayNames,
+                                             @NotNull List<ConstraintParameter> parameters) {
+
+  public ConstraintInstancesResponseDto(List<ConstraintInstance> constraintInstances,
+                                        List<ConstraintArgumentDisplayName> displayNames) {
+    this(constraintInstances.stream().map(SlimInstance::new).toList(),
+        displayNames,
+        constraintInstances.getFirst().getSignature().getParameters());
+  }
+
+  public record SlimInstance(@NotEmpty String id, @NotNull RewardPenalize type,
+                             @NotNull List<SlimArgument> arguments) {
+    private SlimInstance(ConstraintInstance constraintInstance) {
+      this(constraintInstance.getId(),
+          constraintInstance.getType(),
+          constraintInstance.getArguments().stream().map(SlimArgument::new).toList());
+    }
+
+    private record SlimArgument(@NotEmpty String id, @NotNull String value) {
+      private SlimArgument(ConstraintArgument constraintArgument) {
+        this(constraintArgument.getId(),
+            constraintArgument.getValue());
+      }
+    }
+  }
 }
