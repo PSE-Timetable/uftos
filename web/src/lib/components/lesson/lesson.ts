4d59279ac8fac3d3a59c3bfd7c65c4f5706a2159
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2024-07-22 22:05:53 +0200

    feat: generate timetables (#136)
    * feat: generate timetables
    
    * Run npm i
    
    ---------
    
    Co-authored-by: Demartino Marco <marcodemartino23@gmail.com>

---
+++ web/src/lib/components/lesson/lesson.ts
@@ -0,0 +1,2 @@
+export type Color = 'purple' | 'orange' | 'lightBlue' | 'blue' | 'green' | 'red' | 'yellow' | 'brown' | 'aquamarine';
+export type Slot = { text: string; onClick?: () => Promise<void> | void };
