35f378104b3a9122e75c11130263acfd8820bd31
Lars Steudle <uojhg@student.kit.edu>
2024-07-23 20:17:14 +0200

    Removed testing button

--- web/src/routes/admin/(resources)/students/+page.svelte
+++ web/src/routes/admin/(resources)/students/+page.svelte
@@ -1,7 +1,5 @@
 <script lang="ts">
-  import { Button } from '$lib/elements/ui/button';
   import DataTable from '$lib/elements/ui/dataTable/data-table.svelte';
-  import { createStudent, type StudentRequestDto } from '$lib/sdk/fetch-client';
   import { deleteStudentEntry, loadStudentPage } from '$lib/utils/resources';
   import { onMount } from 'svelte';
 
@@ -9,13 +7,6 @@
   let keys = ['id', 'firstName', 'lastName', 'tags'];
   let pageLoaded = false;
   onMount(() => (pageLoaded = true));
-
-  let mmNumber = 0;
-  const create = async () => {
-    let student: StudentRequestDto = { firstName: 'Max' + mmNumber, lastName: 'Mustermann', tagIds: [] };
-    await createStudent(student);
-    mmNumber++;
-  };
 </script>
 
 <div class="p-10 w-full">
@@ -24,5 +15,3 @@
     <DataTable {columnNames} {keys} loadPage={loadStudentPage} deleteEntry={deleteStudentEntry} />
   {/if}
 </div>
-
-<Button on:click={create}>Add</Button>
