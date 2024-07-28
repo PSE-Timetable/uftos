defa0e2d809fdf4ab02657575af1b268f61d6e45
marcelfloe <marcelfloe@gmail.com>
2024-07-28 11:47:06 +0200

    Started implementing consistency check, however solver branch and constraint branch required before able to continue.

--- server/src/main/java/de/uftos/services/UcdlEditorService.java
+++ server/src/main/java/de/uftos/services/UcdlEditorService.java
@@ -1,10 +1,17 @@
 package de.uftos.services;
 
+import de.uftos.dto.ResourceType;
+import de.uftos.dto.ucdl.ConstraintDefinitionDto;
 import de.uftos.dto.ucdl.ParsingResponse;
+import de.uftos.entities.ConstraintSignature;
+import de.uftos.repositories.database.ConstraintSignatureRepository;
 import de.uftos.repositories.ucdl.UcdlRepository;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.util.HashMap;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.InputStreamResource;
 import org.springframework.core.io.Resource;
@@ -19,6 +26,7 @@ import org.springframework.web.server.ResponseStatusException;
 @Service
 public class UcdlEditorService {
   private final UcdlRepository ucdlRepository;
+  private final ConstraintSignatureRepository constraintSignatureRepository;
 
   /**
    * Creates a UCDL editor service.
@@ -26,8 +34,10 @@ public class UcdlEditorService {
    * @param ucdlRepository the repository for accessing the UCDL parser.
    */
   @Autowired
-  public UcdlEditorService(UcdlRepository ucdlRepository) {
+  public UcdlEditorService(UcdlRepository ucdlRepository,
+                           ConstraintSignatureRepository constraintSignatureRepository) {
     this.ucdlRepository = ucdlRepository;
+    this.constraintSignatureRepository = constraintSignatureRepository;
   }
 
   /**
@@ -51,9 +61,54 @@ public class UcdlEditorService {
    * @param force whether inconsistencies should be ignored and the ucdl file forcefully updated.
    * @return a response whether parsing the file was successful or not.
    */
+  //TODO: Wait for Solver and Constraint branches to be merged as a lot of code is currently missing
   public ParsingResponse setUcdl(MultipartFile file, boolean force) {
-    //todo: set String of repository
+    if (force) {
+      return forceSetUcdl(file);
+    }
+    //todo: set String of repository (without force)
     return ucdlRepository.parseFile();
+  }
+
+  private ParsingResponse forceSetUcdl(MultipartFile file) {
+    try {
+      String ucdl = new String(file.getBytes());
+      ucdlRepository.setUcdl(ucdl);
+    } catch (IOException e) {
+      return new ParsingResponse(false, e.getMessage());
+    }
+
+    try {
+      HashMap<String, ConstraintDefinitionDto> definitions = ucdlRepository.getConstraints();
+
+      List<ConstraintSignature> signatures = constraintSignatureRepository.findAll();
+
+      for (ConstraintSignature signature : signatures) {
+        ConstraintDefinitionDto definition = definitions.get(signature.getName());
+
+        //constraint with this signature got deleted or signature got changed
+        if (definition == null
+            || definition.parameters().size() != signature.getParameters().size()) {
+          constraintSignatureRepository.delete(signature);
+        }
+        for (int i = 0; i < signature.getParameters().size(); i++) {
+          //todo (once the missing code is available)
+          ResourceType signatureParameterType = signature.getParameters().get(i).getParameterType();
+          ResourceType definitionParameterType = definition.parameters().get("this");
+          if (signatureParameterType != definitionParameterType) {
+            constraintSignatureRepository.delete(signature);
+
+            ConstraintSignature updatedSignature = new ConstraintSignature();
+
+            break;
+          }
+        }
+      }
+      return new ParsingResponse(true,
+          "Saved file forcefully and deleted inconsistent constraint instances!");
+    } catch (ParseException | IOException e) {
+      return new ParsingResponse(true, "Saved file with invalid code forcefully!");
+    }
   }
 
   /**
