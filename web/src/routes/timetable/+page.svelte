0eafa774baf138e44c6250200fc4ec00f7821baa
Demartino Marco <marcodemartino23@gmail.com>
2024-07-29 15:05:45 +0200

    Update web/src/routes/timetable/+page.svelte
    Co-authored-by: Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>

--- web/src/routes/timetable/+page.svelte
+++ web/src/routes/timetable/+page.svelte
@@ -2,7 +2,5 @@
   import { goto } from '$app/navigation';
   import { onMount } from 'svelte';
 
-  onMount(async () => {
-    await goto('./timetable/teacher');
-  });
+  onMount(() => goto('./timetable/teacher'));
 </script>
