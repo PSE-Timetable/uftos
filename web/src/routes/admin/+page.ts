97cdf9fae41b075e05ef444e5c4eda915d63c962
Lars Steudle <uojhg@student.kit.edu>
2024-08-08 12:58:07 +0200

    Forgot overview page

--- web/src/routes/admin/+page.ts
+++ web/src/routes/admin/+page.ts
@@ -1,14 +1,19 @@
 import { loadGrades, loadRoomPage, loadStudentPage, loadTeacherPage } from '$lib/utils/resources';
+import { error } from '@sveltejs/kit';
 import type { PageLoad } from './$types';
 
 export const load = (async () => {
-  return {
-    initialGrades: await loadGrades('', ''),
-    initialRooms: await loadRoomPage('', '', 0),
-    initialStudents: await loadStudentPage('', '', 0),
-    initialTeachers: await loadTeacherPage('', '', 0),
-    meta: {
-      title: 'Übersicht',
-    },
-  };
+  try {
+    return {
+      initialGrades: await loadGrades('', ''),
+      initialRooms: await loadRoomPage('', '', 0, 15),
+      initialStudents: await loadStudentPage('', '', 0, 15),
+      initialTeachers: await loadTeacherPage('', '', 0, 15),
+      meta: {
+        title: 'Übersicht',
+      },
+    };
+  } catch {
+    error(400, { message: 'Could not fetch page' });
+  }
 }) satisfies PageLoad;
