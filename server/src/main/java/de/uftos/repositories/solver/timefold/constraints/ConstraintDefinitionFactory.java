0d5ce10922c06560c1d66064a04ae2b690e7e4e5
marcelfloe <marcelfloe@gmail.com>
2024-08-12 15:19:22 +0200

    Fixing mistake in AST-translation.

--- server/src/main/java/de/uftos/repositories/solver/timefold/constraints/ConstraintDefinitionFactory.java
+++ server/src/main/java/de/uftos/repositories/solver/timefold/constraints/ConstraintDefinitionFactory.java
@@ -48,17 +48,12 @@ public class ConstraintDefinitionFactory {
       ConstraintDefinitionDto definition) {
     String name = definition.name();
     RewardPenalize defaultType = definition.defaultType();
-    Function<List<ResourceTimefoldInstance>, Boolean> function;
 
     LinkedHashMap<String, ResourceType> params = definition.parameters();
 
     AbstractSyntaxTreeDto ast = definition.root();
 
-    if (ast.getToken() == UcdlToken.CODEBLOCK) {
-      function = convertCodeblock(ast, params);
-    } else {
-      function = convertBool(ast, params);
-    }
+    Function<List<ResourceTimefoldInstance>, Boolean> function = convertBool(ast, params);
 
     return new ConstraintDefinitionTimefoldInstance(name, defaultType, function);
   }
@@ -234,6 +229,7 @@ public class ConstraintDefinitionFactory {
       > convertBool(
       AbstractSyntaxTreeDto ast, LinkedHashMap<String, ResourceType> params) {
     return switch (ast.getToken()) {
+      case CODEBLOCK -> convertCodeblock(ast, params);
       case IMPLIES -> convertImplies(ast, params);
       case OR -> convertOr(ast, params);
       case AND -> convertAnd(ast, params);
@@ -806,9 +802,9 @@ public class ConstraintDefinitionFactory {
       setReference.add(name.apply(arguments));
 
       for (Function<
-            List<ResourceTimefoldInstance>,
-            Function<Set<ResourceTimefoldInstance>, Set<ResourceTimefoldInstance>>
-            > function : modifiers) {
+          List<ResourceTimefoldInstance>,
+          Function<Set<ResourceTimefoldInstance>, Set<ResourceTimefoldInstance>>
+          > function : modifiers) {
         setReference = function.apply(arguments).apply(setReference);
       }
       return setReference;
@@ -1119,9 +1115,9 @@ public class ConstraintDefinitionFactory {
       elementReference.add(name.apply(arguments));
 
       for (Function<
-            List<ResourceTimefoldInstance>,
-            Function<Set<ResourceTimefoldInstance>, Set<ResourceTimefoldInstance>>
-            > function : attributes) {
+          List<ResourceTimefoldInstance>,
+          Function<Set<ResourceTimefoldInstance>, Set<ResourceTimefoldInstance>>
+          > function : attributes) {
         elementReference = function.apply(arguments).apply(elementReference);
       }
       if (elementReference.size() != 1) {
