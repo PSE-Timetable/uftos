3516f640c474a96eb596103653cbcc31610e7a72
marcelfloe <marcelfloe@gmail.com>
2024-08-12 15:58:21 +0200

    Checkstyle.

--- server/src/test/java/de/uftos/repositories/solver/timefold/constraints/ConstraintDefinitionFactoryTest.java
+++ server/src/test/java/de/uftos/repositories/solver/timefold/constraints/ConstraintDefinitionFactoryTest.java
@@ -26,7 +26,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
-@SuppressWarnings("checkstyle:MissingJavadocType")
+@SuppressWarnings("checkstyle:MissingJavadocType,checkstyle:VariableDeclarationUsageDistance")
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class ConstraintDefinitionFactoryTest {
