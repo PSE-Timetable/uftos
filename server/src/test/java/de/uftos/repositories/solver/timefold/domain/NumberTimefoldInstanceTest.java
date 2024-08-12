4c9a7e8550f3f47e511b017caa209d02eb3648df
marcelfloe <marcelfloe@gmail.com>
2024-08-12 13:53:39 +0200

    Created tests for the new methods of NumberTimefoldInstance.

--- server/src/test/java/de/uftos/repositories/solver/timefold/domain/NumberTimefoldInstanceTest.java
+++ server/src/test/java/de/uftos/repositories/solver/timefold/domain/NumberTimefoldInstanceTest.java
@@ -1,6 +1,7 @@
 package de.uftos.repositories.solver.timefold.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import de.uftos.dto.ResourceType;
 import org.junit.jupiter.api.Test;
@@ -32,5 +33,31 @@ public class NumberTimefoldInstanceTest {
 
     number = new NumberTimefoldInstance(Integer.MAX_VALUE);
     assertEquals(number.getId(), "number:" + Integer.MAX_VALUE);
+  }
+
+  @Test
+  void equals() {
+    NumberTimefoldInstance number = new NumberTimefoldInstance(0);
+
+    assertEquals(number, new NumberTimefoldInstance(0));
+    assertEquals(number, number);
+    assertNotEquals(number, null);
+    assertNotEquals(number, new StudentGroupTimefoldInstanceTest());
+    assertNotEquals(number, new NumberTimefoldInstance(1));
+
+  }
+
+  @Test
+  void hashCodeTest() {
+    NumberTimefoldInstance number;
+
+    number = new NumberTimefoldInstance(0);
+    assertEquals(number.hashCode(), number.getValue());
+
+    number = new NumberTimefoldInstance(Integer.MIN_VALUE);
+    assertEquals(number.hashCode(), number.getValue());
+
+    number = new NumberTimefoldInstance(Integer.MAX_VALUE);
+    assertEquals(number.hashCode(), number.getValue());
   }
 }
