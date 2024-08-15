a309e651523832c67e69bfa77cafbd19f0893e48
Demartino Marco <marcodemartino23@gmail.com>
2024-08-15 13:13:32 +0200

    Removed useless repos from test

--- server/src/test/java/de/uftos/services/TimetableServiceTests.java
+++ server/src/test/java/de/uftos/services/TimetableServiceTests.java
@@ -4,17 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
-import de.uftos.entities.Break;
 import de.uftos.entities.Lesson;
-import de.uftos.entities.Server;
 import de.uftos.entities.Timetable;
-import de.uftos.entities.TimetableMetadata;
-import de.uftos.repositories.database.ServerRepository;
 import de.uftos.repositories.database.TimetableRepository;
-import de.uftos.repositories.ucdl.UcdlRepository;
 import de.uftos.repositories.ucdl.parser.javacc.ParseException;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
@@ -34,11 +28,6 @@ public class TimetableServiceTests {
 
   @Mock
   private TimetableRepository timetableRepository;
-  @Mock
-  private UcdlRepository ucdlRepository;
-
-  @Mock
-  private ServerRepository serverRepository;
 
   @InjectMocks
   private TimetableService timetableService;
@@ -57,12 +46,6 @@ public class TimetableServiceTests {
 
     when(timetableRepository.findAll()).thenReturn(List.of(timetable));
     when(timetableRepository.findById("timetable123")).thenReturn(Optional.of(timetable));
-
-    when(ucdlRepository.getConstraints()).thenReturn(new HashMap<>());
-
-    Break[] breaks = new Break[0];
-    Server server = new Server(new TimetableMetadata(45, 10, "07:45", breaks), "2024");
-    when(serverRepository.findAll()).thenReturn(List.of(server));
   }
 
   @Test
