bfff8807cf58774f8137b4d59f73a314c2c75e6e
uhkfc <daniel.dietzler@student.kit.edu>
2024-08-22 14:14:14 +0200

    fix: show page title

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
   import { Toaster } from 'svelte-sonner';
   import '../app.css';
+  import { page } from '$app/stores';
 </script>
+
+<svelte:head>
+  <title>{$page.data.meta.title}</title>
+</svelte:head>
 
 <slot />
 <Toaster class="text-primary" />
