97ff3da649e1a011328a05556a2332df3138c372
Demartino Marco <marcodemartino23@gmail.com>
2024-08-31 07:53:37 +0200

    Added javadoc to test utility class

--- server/src/main/java/de/uftos/controller/SubjectController.java
+++ server/src/main/java/de/uftos/controller/SubjectController.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -83,5 +84,16 @@ public class SubjectController {
   @PutMapping("/{id}")
   public Subject updateSubject(@PathVariable String id, @RequestBody SubjectRequestDto subject) {
     return this.subjectService.update(id, subject);
+  }
+
+  /**
+   * Maps the HTTP DELETE request to the {@link SubjectService#delete(String) delete} function
+   * of the subject service.
+   *
+   * @param ids the ID sof the subject which are to be deleted.
+   */
+  @DeleteMapping()
+  public void deleteSubjects(@RequestBody String[] ids) {
+    this.subjectService.deleteSubjects(ids);
   }
 }
--- server/src/test/java/de/uftos/utils/ClassCaster.java
+++ server/src/test/java/de/uftos/utils/ClassCaster.java
@@ -2,9 +2,25 @@ package de.uftos.utils;
 
 import java.util.List;
 
-@SuppressWarnings("checkstyle:MissingJavadocType")
+/**
+ * Utility class to return the type of any class.
+ * Used when mocking a method that returns a
+ * <pre>
+ * {@code
+ * List<T> list
+ * }
+ * </pre>.
+ */
 public class ClassCaster {
 
+  /**
+   * Returns a class of type
+   * <pre>
+   *  {@code
+   *  List<T> list
+   *  }
+   *  </pre>.
+   */
   @SuppressWarnings("unchecked")
   public static <T> Class<T> getClassType() {
     return (Class<T>) List.class;
