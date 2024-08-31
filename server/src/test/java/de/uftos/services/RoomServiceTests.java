18e32825e5363bf974eb60660b9aec0b051f3f54
Daniel Dietzler <36593685+danieldietzler@users.noreply.github.com>
2024-08-31 13:39:18 +0200

    feat: make timetable work (#391)
    * chore: fix lesson bulk response dto
    
    * feat: make timetable work
    
    * chore: refactor timetable service, start solving with random entities
    
    Generated timetables are now always saved (after all the lessons are created and saved before the solver is started regardless of whether the timetable is valid or not).
    
    Removed unnecessary exception.
    
    Reduced DB calls.
    
    ---------
    
    Co-authored-by: marcelfloe <marcelfloe@gmail.com>

--- server/src/test/java/de/uftos/services/RoomServiceTests.java
+++ server/src/test/java/de/uftos/services/RoomServiceTests.java
@@ -22,6 +22,7 @@ import de.uftos.entities.Student;
 import de.uftos.entities.StudentGroup;
 import de.uftos.entities.Subject;
 import de.uftos.entities.Teacher;
+import de.uftos.entities.Timetable;
 import de.uftos.entities.TimetableMetadata;
 import de.uftos.repositories.database.RoomRepository;
 import de.uftos.repositories.database.ServerRepository;
@@ -95,9 +96,12 @@ public class RoomServiceTests {
 
     Teacher teacher1 = new Teacher("123");
 
-    Lesson lesson1 = createLesson(teacher1, room1, studentGroup1, "2024", subject);
-    Lesson lesson2 = createLesson(teacher1, room1, studentGroup1, "2022", subject);
-    Lesson lesson3 = createLesson(teacher1, room1, studentGroup2, "2024", subject);
+    Timetable timetable = new Timetable("timetable");
+    timetable.setId("timetableId");
+
+    Lesson lesson1 = createLesson(teacher1, room1, studentGroup1, "2024", subject, timetable);
+    Lesson lesson2 = createLesson(teacher1, room1, studentGroup1, "2022", subject, timetable);
+    Lesson lesson3 = createLesson(teacher1, room1, studentGroup2, "2024", subject, timetable);
 
     room1.setLessons(List.of(lesson1, lesson2, lesson3));
 
@@ -189,7 +193,8 @@ public class RoomServiceTests {
     );
 
     assertAll("Testing whether all the rooms are there",
-        () -> assertTrue(result.rooms().stream().map(room -> room.getId()).toList().contains(room1.getId()))
+        () -> assertTrue(
+            result.rooms().stream().map(Room::getId).toList().contains(room1.getId()))
     );
 
     assertAll("Testing whether all the student groups are there",
@@ -203,15 +208,15 @@ public class RoomServiceTests {
     );
   }
 
-  private Lesson createLesson(Teacher teacher, Room room, StudentGroup studentGroup,
-                              String number,
-                              Subject subject) {
+  private Lesson createLesson(Teacher teacher, Room room, StudentGroup studentGroup, String number,
+                              Subject subject, Timetable timetable) {
     Lesson lesson = new Lesson();
     lesson.setTeacher(teacher);
     lesson.setRoom(room);
     lesson.setStudentGroup(studentGroup);
     lesson.setYear(number);
     lesson.setSubject(subject);
+    lesson.setTimetable(timetable);
     return lesson;
   }
 
--- web/src/routes/timetable/teacher/+page.ts
+++
@@ -1,9 +0,0 @@
-import type { PageLoad } from '../../$types';
-
-export const load = (() => {
-  return {
-    meta: {
-      title: 'Teacher timetable',
-    },
-  };
-}) satisfies PageLoad;
