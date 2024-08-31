1b1f9c39d84e27b3f84e934e4fc254af392e6b7b
Lars Steudle <117660926+Lama64@users.noreply.github.com>
2024-08-31 20:17:28 +0200

    Forms in add resource (#396)
    * Changed add resource page to use forms
    
    * Recommendations
    
    * Web tests
    
    * Fixed tests

--- web/src/routes/admin/(resources)/grades/[id]/+page.ts
+++ web/src/routes/admin/(resources)/grades/[id]/+page.ts
@@ -1,39 +1,47 @@
-import { getGrade, getTags, type GradeResponseDto, type Sort } from '$lib/sdk/fetch-client';
+import { getGrade, getTags, type Sort } from '$lib/sdk/fetch-client';
 import { error } from '@sveltejs/kit';
+import { superValidate } from 'sveltekit-superforms';
+import { zod } from 'sveltekit-superforms/adapters';
+import { z } from 'zod';
 import type { PageLoad } from './$types';
+
+export const _schema = z.object({
+  id: z.string(),
+  name: z.string().trim().min(1, { message: 'Der Name darf nicht leer sein.' }),
+  tags: z.string().array(),
+  studentGroups: z.string().array(),
+});
 
 export const load = (async ({ params }) => {
   const sort: Sort = { sort: ['name,asc'] };
   const tags = await getTags(sort);
+  let formGrade: { id: string; name: string; studentGroups: string[]; tags: string[] }, title: string;
   if (params.id === 'new') {
-    const grade: GradeResponseDto = {
+    formGrade = {
       id: 'new',
-      curriculumId: '',
       name: '',
-      studentGroupIds: [],
-      studentIds: [],
+      studentGroups: [],
       tags: [],
     };
-    return {
-      grade,
-      tags,
-      create: true,
-      meta: {
-        title: `Stufe — Hinzufügen`,
-      },
-    };
+    title = `Stufe — Hinzufügen`;
+  } else {
+    try {
+      const grade = await getGrade(params.id);
+      formGrade = {
+        ...grade,
+        tags: grade.tags.map((tag) => tag.id),
+        studentGroups: grade.studentGroupIds,
+      };
+      title = `Stufe — ${grade.name}`;
+    } catch {
+      error(404, { message: `Grade with id ${params.id} not found` });
+    }
   }
-  try {
-    const grade = await getGrade(params.id);
-    return {
-      grade,
-      tags,
-      create: false,
-      meta: {
-        title: `Stufe — ${grade.name}`,
-      },
-    };
-  } catch {
-    error(404, { message: `Grade with id ${params.id} not found` });
-  }
+  return {
+    form: await superValidate(formGrade, zod(_schema), { errors: false }),
+    tags,
+    meta: {
+      title,
+    },
+  };
 }) satisfies PageLoad;
