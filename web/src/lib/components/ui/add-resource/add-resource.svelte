af4246a543fd635cf9cf12e61293c1cb84fcdab4
Demartino Marco <marcodemartino23@gmail.com>
2024-09-01 22:45:41 +0200

    fix: css (#425)
    * fix css
    
    * new fixes

--- web/src/lib/components/ui/add-resource/add-resource.svelte
+++ web/src/lib/components/ui/add-resource/add-resource.svelte
@@ -55,9 +55,9 @@
 </script>
 
 <form method="POST" use:enhance>
-  <div class="grid grid-cols-[max-content,1fr] gap-8 p-4">
+  <div class="grid grid-cols-[max-content,1fr] items-center gap-8 p-4">
     {#each descriptions as description, i}
-      <div class="text-lg font-bold flex">{description}</div>
+      <div class="text-base font-bold flex items-center">{description}</div>
       <div class="flex flex-col gap-1 w-80">
         <Form.Field {form} name={names[i]}>
           <Form.Control let:attrs>
@@ -92,7 +92,7 @@
     {/if}
 
     {#if names.includes('capacity')}
-      <div class="text-lg font-bold flex">Kapazität:</div>
+      <div class="text-base font-bold flex items-center">Kapazität:</div>
       <div class="flex flex-col gap-1 w-80">
         <Form.Field {form} name="capacity">
           <Form.Control let:attrs>
@@ -111,7 +111,7 @@
     {/if}
 
     {#if grades}
-      <div class="flex text-lg font-bold">Stufe:</div>
+      <div class="flex text-base font-bold items-center">Stufe:</div>
       {#if gradesAvailable}
         <div class="flex flex-col">
           <Form.Field {form} name="grades">
@@ -127,12 +127,12 @@
           </Form.Field>
         </div>
       {:else}
-        <div class="text-lg font-semibold text-red-600">Es müssen Grades vorhanden sein.</div>
+        <div class="text-base font-semibold text-red-600">Es müssen Grades vorhanden sein.</div>
       {/if}
     {/if}
 
     {#if subjects}
-      <div class="flex text-lg font-bold">Fächer:</div>
+      <div class="flex text-base font-bold items-center">Fächer:</div>
       {#if subjects.length > 0}
         <div class="flex flex-wrap bg-white rounded-md gap-2 p-4 shadow-custom max-w-80">
           {#each subjects as subject, i}
@@ -145,14 +145,14 @@
           {/each}
         </div>
       {:else}
-        <div class="text-lg font-semibold">Keine Fächer vorhanden.</div>
+        <div class="text-base font-semibold">Keine Fächer vorhanden.</div>
       {/if}
     {/if}
 
     {#if tags}
-      <div class=" flex text-lg font-bold">Tags:</div>
+      <div class=" flex text-base font-bold">Tags:</div>
       {#if tags.length > 0}
-        <div class="w-80 flex">
+        <div class="w-80 flex items-center">
           <Form.Field {form} name="tags">
             <Form.Control>
               <TagsMultipleSelect {tags} bind:selectedTagIds={$formData.tags} />
@@ -160,13 +160,13 @@
           </Form.Field>
         </div>
       {:else}
-        <div class="text-lg font-semibold">Keine Tags vorhanden.</div>
+        <div class="text-base font-semibold">Keine Tags vorhanden.</div>
       {/if}
     {/if}
 
     <Form.Button
       on:click={() => handleSubmit()}
-      class="col-start-2 p-8 text-lg w-80 bg-accent text-white hover:bg-accent flex"
+      class="col-start-2 py-8 text-base w-80 bg-accent text-white hover:bg-accent flex"
       variant="secondary"
       disabled={grades && !gradesAvailable}
     >
