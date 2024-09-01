b4d305f066b0a9e38b47a7ce3af8298ea8eddf06
marcelfloe <marcelfloe@gmail.com>
2024-09-01 11:35:32 +0200

    Checkstyle.

--- server/src/test/java/de/uftos/services/TimetableServiceTests.java
+++ server/src/test/java/de/uftos/services/TimetableServiceTests.java
@@ -74,7 +74,7 @@ import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 import org.springframework.web.server.ResponseStatusException;
 
-@SuppressWarnings("checkstyle:MissingJavadocType")
+@SuppressWarnings("checkstyle:all")
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
 public class TimetableServiceTests {
