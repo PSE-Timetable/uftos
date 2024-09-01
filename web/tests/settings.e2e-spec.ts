3ed70d2108d83266db225a90ce2ea4262be4e929
Lars Steudle <uojhg@student.kit.edu>
2024-09-01 15:25:02 +0200

    Adjusted for changes to settings

--- web/tests/settings.e2e-spec.ts
+++ web/tests/settings.e2e-spec.ts
@@ -7,31 +7,36 @@ test.describe.configure({ mode: 'serial' });
 test.describe('Settings', () => {
   test.beforeAll('delete all existing timeslots', async ({ browser }) => {
     page = await browser.newPage();
-    await page.goto('http://localhost:5173/');
+    await page.goto('/');
     await page.getByRole('link').first().click();
-    await page.waitForURL('http://localhost:5173/admin');
+    await page.waitForURL('/admin');
     await expect(async () => {
       await page.getByRole('button', { name: 'Einstellungen' }).click({ timeout: 500 });
       await page.getByRole('menuitem', { name: 'Allgemeine Einstell.' }).click({
         timeout: 1250,
       });
-      await page.waitForURL('http://localhost:5173/admin/settings');
+      await page.waitForURL('/admin/settings');
     }).toPass();
-    await page.getByPlaceholder('Anzahl').click();
-    await page.getByPlaceholder('Anzahl').fill('0');
     await expect(async () => {
-      await expect(page.getByPlaceholder('Länge')).toBeEditable();
-      await page.getByPlaceholder('Länge').click();
-      await page.getByPlaceholder('Länge').fill('45');
-      await expect(page.getByPlaceholder('Länge')).toHaveValue('45');
+      await expect(page.getByPlaceholder('Anzahl')).toBeEditable();
+      await page.getByPlaceholder('Anzahl').click();
+      await page.getByPlaceholder('Anzahl').fill('0');
+      await expect(page.getByPlaceholder('Anzahl')).toHaveValue('0', { timeout: 250 });
+      await expect(page.locator('body')).not.toContainText('1. Stunde', { timeout: 250 });
+    }).toPass();
+    await expect(async () => {
+      await expect(page.getByPlaceholder('Länge').first()).toBeEditable();
+      await page.getByPlaceholder('Länge').first().click();
+      await page.getByPlaceholder('Länge').first().fill('45');
+      await expect(page.getByPlaceholder('Länge').first()).toHaveValue('45');
     }).toPass();
     await page.getByPlaceholder(':45').click();
     await page.getByPlaceholder(':45').fill('08:00');
     await expect(async () => {
-      await expect(page.getByPlaceholder('Länge')).toBeEditable();
-      await page.getByPlaceholder('Länge').click();
-      await page.getByPlaceholder('Länge').fill('45');
-      await expect(page.getByPlaceholder('Länge')).toHaveValue('45');
+      await expect(page.getByPlaceholder('Länge').first()).toBeEditable();
+      await page.getByPlaceholder('Länge').first().click();
+      await page.getByPlaceholder('Länge').first().fill('45');
+      await expect(page.getByPlaceholder('Länge').first()).toHaveValue('45');
     }).toPass();
   });
 
@@ -76,8 +81,12 @@ test.describe('Settings', () => {
   });
 
   test('change timeslot length', async () => {
-    await page.getByLabel('Timeslots Länge:').click();
-    await page.getByLabel('Timeslots Länge:').fill('60');
+    await expect(async () => {
+      await page.getByLabel('Timeslots Länge:').click();
+      await page.getByLabel('Timeslots Länge:').fill('60');
+      await expect(page.getByLabel('Timeslots Länge:')).toHaveValue('60');
+    }).toPass();
+
     await expect(page.locator('body')).toContainText('1. Stunde von 08:00 bis 09:00');
     await expect(page.locator('body')).toContainText('2. Stunde von 09:00 bis 10:00');
     await expect(page.locator('body')).toContainText('Pause von 10:00 bis 10:20');
@@ -88,6 +97,7 @@ test.describe('Settings', () => {
     await expect(page.locator('body')).toContainText('Pause von 14:30 bis 15:30');
     await expect(page.locator('body')).toContainText('7. Stunde von 15:30 bis 16:30');
     await expect(page.locator('body')).toContainText('8. Stunde von 16:30 bis 17:30');
+    await expect(page.getByRole('button', { name: 'Speichern' })).toBeEnabled();
     await page.getByRole('button', { name: 'Speichern' }).click();
   });
 
